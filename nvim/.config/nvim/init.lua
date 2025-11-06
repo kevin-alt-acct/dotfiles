7cd9e36fcc469bfa5857eed901f7d1e79143509e
Kevin Moritz <kevin@quavered.com>
2025-11-06 08:03:44 -0600

    chore: nvim lspconfig fix

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -218,7 +218,7 @@ require("lazy").setup({
       { "j-hui/fidget.nvim", opts = {} },
       { "folke/neodev.nvim", opts = {} },
     },
-    commit = "main",
+    commit = "master",
     config = function()
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
